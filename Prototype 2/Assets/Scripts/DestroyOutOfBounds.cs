008862bf12744f3dd37863165b1ad284be5bce66
Eric Kim <erickim@Erics-MacBook-Air.local>
2020-06-20 21:51:24 -0700

    Finished up to 5.2

---
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 30;
+    private float lowerBound = -10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Sets bound for pizza
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        //Sets bound for animals
+        else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+    }
+}
